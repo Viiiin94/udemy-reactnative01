19748191650bf84c2e2ee9a456870a6c67e69fa3
Viiiin94 <mynameisyyb@naver.com>
2025-09-08 16:04:38 +0900

    네비게이션 중첩 Stack -> Drawer

--- App.js
+++ App.js
@@ -2,18 +2,40 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import CategoriesScreen from './screen/CategoriesScreen';
 import MealsOverViewScreen from './screen/MealsOverViewScreen';
 import MealDetailScreen from './screen/MealDetailScreen';
+import FavoritesScreen from './screen/FavoritesScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#351401' },
+        headerTintColor: 'white',
+        contentStyle: { backgroundColor: '#3f2f25' },
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{ title: 'All Categories' }}
+      />
+      <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
-        <StatusBar style="dark" />
+        <StatusBar style="dark" />s
         <NavigationContainer>
           <Stack.Navigator
             screenOptions={{
@@ -23,10 +45,10 @@ export default function App() {
             }}
           >
             <Stack.Screen
-              name="MealsCategories"
-              component={CategoriesScreen}
+              name="Drawer"
+              component={DrawerNavigator}
               options={{
-                title: 'All Categories',
+                headerShown: false,
               }}
             />
             <Stack.Screen
